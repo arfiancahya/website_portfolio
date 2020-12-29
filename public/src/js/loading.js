b160fe44b90710afe2dcc48633b7bd8d61200a19
arfiancahya <arfian.dwisetya@gmail.com>
2020-12-29 13:13:19 +0700

    Update animation

--- public/src/js/loading.js
+++ public/src/js/loading.js
@@ -2,3 +2,7 @@ window.addEventListener('load', () => {
     const preload = document.querySelector('.loading');
     preload.classList.add('loading-finish');
 })
+
+const time = gsap.timeline({ default: { ease: 'power1.out' } });
+time.fromTo("#div1", { opacity: 0 }, { opacity: 1, duration: 1 });
+time.fromTo(".home__content", { opacity: 0 }, { opacity: 1, duration: 1.1 }, "-=1");
