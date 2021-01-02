c651f8e764184710f2d9fe143287005b7f67a9b5
arfiancahya <arfian.dwisetya@gmail.com>
2021-01-02 17:06:11 +0700

    Post To Email

---
+++ public/kontak.php
@@ -0,0 +1,57 @@
+<?php
+
+if($_POST["submit"]) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $email_title = "";
+    $visitor_message = "";
+    $email_body = "<div>";
+    $recipient = "fiancahya51@gmail.com";
+
+    if(isset($_POST['visitor_name'])) {
+        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+        $email_body.= "<div>
+                        <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+                    </div>";
+    }
+    if(isset($_POST['visitor_email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
+                        </div>";
+    }
+    if(isset($_POST['email_title'])) {
+        $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Reason For Contacting Me:</b></label>&nbsp;<span>".$email_title."</span>
+                        </div>";
+    }
+    if(isset($_POST['visitor_message'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$visitor_message."</div>
+                        </div>";
+    }
+
+    $email_body .= "</div>";
+ 
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+      
+    if(mail($recipient, $email_title, $email_body, $headers)) {
+        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+      
+} else {
+    echo '<p>Something went wrong</p>';
+}
+
+
+
+
+?>
